18c1a6c9c458e96862480b355fd9fdbce786f273
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-01-17 00:59:24 -0800

    rvm gemset creator

---
+++ rvm_create.rb
@@ -0,0 +1,14 @@
+# Creates an rvm gemset w/ rails using supplied arguments
+# TODO?: Does not check for RVM installation,
+#       Should check to see if user wants documentation,
+#       Rails version not specified
+# Usage: $rvm_create 1.9.3 newproject 
+
+raise "Not enough arguments." unless ARGV.length == 2
+ruby_version = ARGV.first
+gemset_name = ARGV.last
+
+system("rvm #{ruby_version} do rvm #{ruby_version}@#{gemset_name} --create")
+puts "Installing current version of Rails"
+system("rvm #{ruby_version}@#{gemset_name} do gem install rails --no-rdoc --no-ri")
+puts "Rails installed with #{ruby_version} at gemset #{gemset_name}"
